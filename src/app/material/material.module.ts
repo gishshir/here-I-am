ce0a19ed9d2174ab9c902f086107cc7ba3c2b1b8
gishshir <gishshir@laposte.net>
2020-07-18 15:58:50 +0200

    notitication avec material snack-bar module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,12 +5,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatButtonToggleModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatButtonToggleModule, MatIconModule]
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatButtonToggleModule, MatIconModule,
+    MatSnackBarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatButtonToggleModule, MatIconModule,
+    MatSnackBarModule]
 })
 export class MaterialModule { }
